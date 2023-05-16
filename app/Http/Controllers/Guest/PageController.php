4a903f632e6ecd5d0dad547c740589d52e72f2a7
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-16 16:20:28 +0200

    base

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Comic;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+
+                $comics = Comic::all();
+
+        return view('welcome',compact('comics'));
+    }
+}
