ca2dbddbeec511154f7bb234d8905cc7a85b5823
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-17 18:13:58 +0200

    migliorato codice, prima non predeva l'id

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -19,34 +19,74 @@
                     <div class="d-flex justify-content-around">
                         <a class="btn btn-success" href="{{ route('comics.show', ['comic' => $comic->id]) }}">Vedi</a>
                         <a class="btn btn-light" href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica</a>
-                        <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
 
-                            <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">
-                                Elimina
-                            </button>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal{{ $comic->id }}">
+                            Elimina
+                        </button>
 
-                            <!-- Modal -->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                                aria-hidden="true">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-body">
-                                            Sei sicuro di voler eliminare questo fumetto?
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-primary"
-                                                data-bs-dismiss="modal">No</button>
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal{{ $comic->id }}" tabindex="-1"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-body">
+                                        Sei sicuro di voler cancellare questo fumetto?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                                        <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}"
+                                            method="POST">
+                                            @csrf
+                                            @method('DELETE')
                                             <button type="submit" class="btn btn-danger">SI</button>
-                                        </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
-                            {{-- <button type="submit" class="btn btn-danger">Elimina</button> --}}
-                        </form>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        {{-- <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST"> @csrf
+                                @method('DELETE')
+
+                                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                                    data-bs-target="#exampleModal"
+                                                    data-bs-target="#modal{{ $comic->id }}">
+                                                    Elimina
+                                                </button>
+
+                                                <div class="modal fade" id="exampleModal" tabindex="-1"
+                                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-body">
+                                                                Sei sicuro di voler eliminare questo fumetto?
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-primary"
+                                                                    data-bs-dismiss="modal">No</button>
+                                                                <button type="submit" class="btn btn-danger">SI</button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                            </form> --}}
                     </div>
                 </div>
             </div>
